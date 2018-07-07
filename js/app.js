ce5d225631eb2a07cf5cd823f952e2df2ae8d8e0
Koczka Zoltán <koczkazoltan@gmx.com>
2018-07-07 19:02:46 +0200

    teszt

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
 // ide deklaráljátok a függvényeket.
 
+function ascendingOrderByCost(data) {
+  var i;
+  var sorted = false;
+  do {
+    sorted = true
+    for (i = 0; i < data.length; i++) {
+     // if (data[i].hasOwnProperty(cost_in_credits)&&data[i+1].hasOwnProperty(cost_in_credits)) {
+        if (parseInt(data[i].id) < parseInt(data[i + 1].id)){
+          let temp = data[i];
+          data[i] = data[i + 1];
+          data[i + 1] = temp;
+          sorted = false
+        }
+      
+    } 
+  }while (!sorted); return data
+}
+
+function sortmax(data) {
+  
+  var i; var j; var end;
+  var count;
+  end = data.length;
+  while (end--) {
+    for (i = 0, j = 1; i < end; i++, j++) {
+      if (parseInt(data[i].id) < parseInt(data[j].id)) {
+        temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+        count++
+      }
+    } 
+  } return count;
+}
+;
+
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -16,5 +52,11 @@ function successAjax(xhttp) {
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
   console.log(userDatas);
+  console.log(ascendingOrderByCost(userDatas))
+  console.log(Object.keys(userDatas))
+  
+ 
+
+ 
 }
-getData('/json/spaceships.json', successAjax);
+  getData('/json/spaceships.json', successAjax);
